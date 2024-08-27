65b570de21194d83b3b7b681d59985857e7b7936
nitesh <Nitesh@git>
2024-08-27 16:28:14 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int num=5;
-            System.out.println(num);
+        System.out.println("latest commit");
     }
 }
