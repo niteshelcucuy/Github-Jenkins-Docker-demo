e5a4b3bf00f567bce60e6238affb05ec43c388b8
nitesh <Nitesh@git>
2024-08-27 16:49:15 +0530

    jenkins docker build configured

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("latest commit");
+        System.out.println("docker file added and jenkins configured");
+
     }
 }
